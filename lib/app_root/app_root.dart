737d03063ca2ea832cc93960a658cb7c2a5036cf
KingNarmar <adlymina99@gmail.com>
2026-04-16 23:14:00 +0200

    Add welcome background image and initial app structure
    - Added a new SVG file for the welcome background image.
    - Created the main application class 'Se7ety' in app_root.dart with a basic MaterialApp.router setup.

---
+++ lib/app_root/app_root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Se7ety extends StatelessWidget {
+  const Se7ety({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
