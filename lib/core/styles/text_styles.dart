bab08587c7e5b0e7237069f96a60c41d94a3e7e9
KingNarmar <adlymina99@gmail.com>
2026-04-17 00:47:03 +0200

    Add font constants and text styles for consistent typography
    - Created AppFonts class to define font family constants.
    - Introduced TextStyles class with various TextStyle constants for different font weights and sizes.

---
+++ lib/core/styles/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const w500s50 = TextStyle(fontWeight: FontWeight.w500, fontSize: 50);
+  static const w700s56 = TextStyle(fontWeight: FontWeight.w700, fontSize: 56);
+  static const w400s47 = TextStyle(fontWeight: FontWeight.w400, fontSize: 47);
+  static const w700s114 = TextStyle(fontWeight: FontWeight.w700, fontSize: 114);
+  static const w500s47 = TextStyle(fontWeight: FontWeight.w500, fontSize: 47);
+  static const w400s44 = TextStyle(fontWeight: FontWeight.w400, fontSize: 44);
+  static const w400s49 = TextStyle(fontWeight: FontWeight.w400, fontSize: 49);
+  static const w400s50 = TextStyle(fontWeight: FontWeight.w400, fontSize: 50);
+  static const w400s36 = TextStyle(fontWeight: FontWeight.w400, fontSize: 36);
+
+ 
+}
