52f9431d03a797cd39caa86d45a4d168726bd042
KingNarmar <adlymina99@gmail.com>
2026-04-21 02:56:13 +0200

    feat: Enhance authentication flow with Firestore integration and shared preferences management

--- lib/features/auth/data/repo/auth_repo.dart
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -1,21 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:se7ety/core/service/firebase/failuer/failuer.dart';
+import 'package:se7ety/core/service/firebase/firestore_provider.dart';
+import 'package:se7ety/core/service/local/shared_pref.dart';
 import 'package:se7ety/features/auth/data/models/auth_params.dart';
+import 'package:se7ety/features/auth/data/models/doctor_model.dart';
+import 'package:se7ety/features/auth/data/models/patient_model.dart';
 
 abstract class AuthRepo {
   static Future<Either<Failuer, String>> login({
     required AuthParams params,
   }) async {
     try {
-      final credential = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(
-            email: params.email,
-            password: params.password,
-          );
-     if(credential.user?.photoURL == "patient"){
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: params.email,
+        password: params.password,
+      );
+
+      await SharedPref.cacheUserId(credential.user?.uid ?? '');
+
+      if (credential.user?.photoURL == "patient") {
         return const Right("مريض");
-      } else if(credential.user?.photoURL == "doctor"){
+      } else if (credential.user?.photoURL == "doctor") {
         return const Right("طبيب");
       }
       return Left(Failuer(message: 'مستخدم غير معروف'));
@@ -45,6 +51,15 @@ abstract class AuthRepo {
           );
       await credential.user?.updateDisplayName(params.name);
       await credential.user?.updatePhotoURL("patient");
+      await SharedPref.cacheUserId(credential.user?.uid ?? '');
+
+      var patientData = PatientModel(
+        name: params.name,
+        email: params.email,
+        uid: credential.user?.uid,
+      );
+      await FirestoreProvider.addPatient(patientData);
+
       return const Right(unit);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
@@ -72,6 +87,13 @@ abstract class AuthRepo {
           );
       await credential.user?.updateDisplayName(params.name);
       await credential.user?.updatePhotoURL("doctor");
+      await SharedPref.cacheUserId(credential.user?.uid ?? '');
+      var doctorDate = DoctorModel(
+        name: params.name,
+        email: params.email,
+        uid: credential.user?.uid,
+      );
+      await FirestoreProvider.addDoctor(doctorDate);
       return const Right(unit);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:se7ety/app_root/app_root.dart';
+import 'package:se7ety/core/service/local/shared_pref.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await SharedPref.init();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Se7ety());
 }
